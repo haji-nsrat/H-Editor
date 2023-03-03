7f424764469d46be49662bba3d075c95be915c73
haji-nsrat <hajibadi18@outlook.com>
2023-03-03 15:14:10 +0300

    Add project files.

---
+++ H-Editor/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace H_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            newToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            editToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            panel_files = new Panel();
+            treeView1 = new TreeView();
+            imageList1 = new ImageList(components);
+            button1 = new Button();
+            panel_text = new Panel();
+            textBox1 = new TextBox();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            panel_files.SuspendLayout();
+            panel_text.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel1.BackColor = SystemColors.Control;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(menuStrip1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 32);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(4, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(29, 32);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top;
+            label1.AutoSize = true;
+            label1.Location = new Point(376, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Editor";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = SystemColors.Control;
+            menuStrip1.Dock = DockStyle.None;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, editToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(37, 5);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(128, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, newToolStripMenuItem, saveToolStripMenuItem, exitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(180, 22);
+            openToolStripMenuItem.Text = "open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // newToolStripMenuItem
+            // 
+            newToolStripMenuItem.Name = "newToolStripMenuItem";
+            newToolStripMenuItem.Size = new Size(180, 22);
+            newToolStripMenuItem.Text = "new";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(180, 22);
+            saveToolStripMenuItem.Text = "save";
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(180, 22);
+            exitToolStripMenuItem.Text = "exit";
+            exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+            // 
+            // editToolStripMenuItem
+            // 
+            editToolStripMenuItem.Name = "editToolStripMenuItem";
+            editToolStripMenuItem.Size = new Size(39, 20);
+            editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(44, 20);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // panel_files
+            // 
+            panel_files.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            panel_files.BackColor = SystemColors.ControlLight;
+            panel_files.Controls.Add(treeView1);
+            panel_files.Controls.Add(button1);
+            panel_files.Location = new Point(0, 0);
+            panel_files.Margin = new Padding(0);
+            panel_files.Name = "panel_files";
+            panel_files.Size = new Size(239, 469);
+            panel_files.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            treeView1.BackColor = SystemColors.ControlLight;
+            treeView1.BorderStyle = BorderStyle.FixedSingle;
+            treeView1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            treeView1.Location = new Point(0, 0);
+            treeView1.Name = "treeView1";
+            treeView1.Size = new Size(239, 469);
+            treeView1.StateImageList = imageList1;
+            treeView1.TabIndex = 1;
+            treeView1.Visible = false;
+            treeView1.NodeMouseClick += treeView1_NodeMouseClick;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "folder.png");
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            button1.BackColor = SystemColors.Highlight;
+            button1.FlatAppearance.BorderColor = SystemColors.HotTrack;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseDownBackColor = SystemColors.Control;
+            button1.FlatAppearance.MouseOverBackColor = SystemColors.Control;
+            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(32, 52);
+            button1.Name = "button1";
+            button1.Size = new Size(172, 30);
+            button1.TabIndex = 0;
+            button1.Text = "Open Folder";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel_text
+            // 
+            panel_text.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            panel_text.BackColor = SystemColors.ScrollBar;
+            panel_text.Controls.Add(textBox1);
+            panel_text.Controls.Add(panel_files);
+            panel_text.Location = new Point(0, 32);
+            panel_text.Name = "panel_text";
+            panel_text.Size = new Size(800, 469);
+            panel_text.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.BackColor = SystemColors.HighlightText;
+            textBox1.Location = new Point(239, 0);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ScrollBars = ScrollBars.Both;
+            textBox1.Size = new Size(561, 469);
+            textBox1.TabIndex = 0;
+            textBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(800, 500);
+            ControlBox = false;
+            Controls.Add(panel_text);
+            Controls.Add(panel1);
+            MainMenuStrip = menuStrip1;
+            MinimumSize = new Size(500, 500);
+            Name = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            panel_files.ResumeLayout(false);
+            panel_text.ResumeLayout(false);
+            panel_text.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem newToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem editToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private PictureBox pictureBox1;
+        private Panel panel_files;
+        private Panel panel_text;
+        private Button button1;
+        private TextBox textBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private TreeView treeView1;
+        private ImageList imageList1;
+    }
+}
---
+++ H-Editor/Form1.cs
@@ -0,0 +1,87 @@
+namespace H_Editor
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1_Click(sender, e);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            treeView1.Nodes.Clear();
+
+            foreach (var item in Directory.GetDirectories(folderBrowserDialog1.SelectedPath))
+            {
+                DirectoryInfo id = new DirectoryInfo(item);
+                var node = treeView1.Nodes.Add(id.Name, id.Name, 0, 0);
+                node.Tag = id;
+            }
+            foreach (var item in Directory.GetFiles(folderBrowserDialog1.SelectedPath))
+            {
+                FileInfo id = new FileInfo(item);
+                var node = treeView1.Nodes.Add(id.Name, id.Name, 0, 0);
+                node.Tag = id;
+            }
+            treeView1.Visible = true;
+        }
+
+        private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+
+            if (e.Node.GetType() == null)
+            {
+                // return
+            }
+            else if (e.Node.Tag.GetType() == typeof(DirectoryInfo))
+            {
+                e.Node.Nodes.Clear();
+
+                foreach (var item in Directory.GetDirectories(((DirectoryInfo)e.Node.Tag).FullName))
+                {
+                    DirectoryInfo id = new DirectoryInfo(item);
+                    var node = e.Node.Nodes.Add(id.Name, id.Name, 0, 0);
+                    node.Tag = id;
+                }
+                foreach (var item in Directory.GetFiles(((DirectoryInfo)e.Node.Tag).FullName))
+                {
+                    FileInfo id = new FileInfo(item);
+                    var node = e.Node.Nodes.Add(id.Name, id.Name, 0, 0);
+                    node.Tag = id;
+                }
+            }
+            else
+            {
+                textBox1.Visible = true;
+            }
+        }
+    }
+}
